2462bdddb9f5c39787a84506a76f46cba1b5bdc0
Juan Antonio <juanfalhqz@gmail.com>
2024-04-06 16:03:03 +0200

    Creating the base of the application.

---
+++ config/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(jlaa*tkhq+clvsjj=#3rnhl*6t8qn$pi2#3(7hv%jnbh!p2c6'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
